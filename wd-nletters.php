41e9105b982e0f6047da3680a8061e4ddfbcfaf2
Webdesign29 <ben@webdesign29.net>
2012-12-17 13:55:17 +0100

    Updating Github Information

--- wd-nletters.php
+++ wd-nletters.php
@@ -24,14 +24,14 @@ function github_plugin_updater_test_init() {
 
     $config = array(
       'slug' => plugin_basename(__FILE__),
-      'proper_folder_name' => 'github-updater',
-      'api_url' => 'https://api.github.com/repos/jkudish/WordPress-GitHub-Plugin-Updater',
-      'raw_url' => 'https://raw.github.com/jkudish/WordPress-GitHub-Plugin-Updater/master',
-      'github_url' => 'https://github.com/jkudish/WordPress-GitHub-Plugin-Updater',
-      'zip_url' => 'https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/zipball/master',
+      'proper_folder_name' => 'wd-mailing',
+      'api_url' => 'https://api.github.com/repos/benfavre/wd-mailing',
+      'raw_url' => 'https://raw.github.com/benfavre/wd-mailing/master',
+      'github_url' => 'https://github.com/benfavre/wd-mailing',
+      'zip_url' => 'https://github.com/benfavre/wd-mailingr/zipball/master',
       'sslverify' => true,
       'requires' => '3.0',
-      'tested' => '3.3',
+      'tested' => '3.5',
       'readme' => 'README.md',
       'access_token' => '',
     );
@@ -39,26 +39,6 @@ function github_plugin_updater_test_init() {
     new WPGitHubUpdater($config);
 
   }
-
-
-
-include_once( WDNLETTER_DIR . '/_github-updater/updater.php');
-if (is_admin()) { // note the use of is_admin() to double check that this is happening in the admin
-    $config = array(
-        'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
-        'proper_folder_name' => 'plugin-name', // this is the name of the folder your plugin lives in
-        'api_url' => 'https://api.github.com/repos/username/repository-name', // the github API url of your github repo
-        'raw_url' => 'https://raw.github.com/username/repository-name/master', // the github raw url of your github repo
-        'github_url' => 'https://github.com/username/repository-name', // the github url of your github repo
-        'zip_url' => 'https://github.com/username/repository-name/zipball/master', // the zip url of the github repo
-        'sslverify' => true // wether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
-        'requires' => '3.0', // which version of WordPress does your plugin require?
-        'tested' => '3.3', // which version of WordPress is your plugin tested up to?
-        'readme' => 'README.md', // which file to use as the readme for the version number
-        'access_token' => '', // Access private repositories by authorizing under Appearance > Github Updates when this example plugin is installed
-    );
-    new WPGitHubUpdater($config);
-}
 
 include_once( WDNLETTER_DIR . '/inc/emogrifier.php');
 include_once( WDNLETTER_DIR . '/inc/class.html2text.php');
